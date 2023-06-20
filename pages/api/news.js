001f99b4e032a79430060f093316d6752163cb97
Charckes <charlesmochss@gmail.com>
2023-06-20 17:09:28 -0300

    .env arrumado

--- pages/api/news.js
+++ pages/api/news.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
 export default async function handler(req, res) {
+    const api_key = process.env.API_KEY
     const { data } = await axios.get('https://newsapi.org/v2/top-headlines', {
         params: {
-            country: 'br',
-            apiKey: '2e2a6c34a6c643c2931f01de92ecf374'
+            country: 'us',
+            apiKey: api_key,
         }
     })
 
